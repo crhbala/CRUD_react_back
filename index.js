f3037f2486e6df1cf7c96d9e3c1f93fc31f0d9e7
crhbala <chrisbala8888@gmail.com>
2024-01-26 16:56:04 +0530

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import AppRoutes from './src/routes/root.js'
 import dotenv from 'dotenv'
 import express from 'express'
+import cors from 'cors'
 
 dotenv.config()
 const PORT = process.env.PORT
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 
